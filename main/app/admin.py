f1aae95d4d556fbe1e2715d46c657af237770f41
Your Name <you@example.com>
2021-02-14 21:45:00 +0300

    asd

--- main/app/admin.py
+++ main/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User_Gl,Trener,User
+
+
+admin.site.register(User_Gl)
+admin.site.register(Trener)
+admin.site.register(User)
--- main/app/models.py
+++ main/app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class User_Gl(models.Model):
+	name = models.CharField(max_length=300)
+	sername = models.CharField(max_length=300)
+	othername = models.CharField(max_length=300)
+	age = models.IntegerField()
+	adress = models.CharField(max_length=300)
+	poster = models.ImageField(upload_to = 'poster/')
+class Trener(models.Model):
+	poster = ForeignKey(poster,on_delete =SET_NULL)
+	staj = IntegerField()
+	salary = IntegerField()
+	time_work = models.TimeField()
+	name = ForeignKey(User_Gl,on_delete = CASCADE)
+	sername = ForeignKey(User_Gl,on_delete = CASCADE)
+	othername = ForeignKey(User_Gl,on_delete = CASCADE)
+	age = ForeignKey(User_Gl,on_delete = CASCADE)
+	adress = ForeignKey(User_Gl,on_delete = CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	def __str__(self):
+		return f'{self.name} {self.sername}'
+class User(models.Model):
+	poster = ForeignKey(poster,on_delete = SET_NULL)
+	name = ForeignKey(User_Gl,on_delete = CASCADE)
+	sername = ForeignKey(User_Gl,on_delete = CASCADE)
+	othername = ForeignKey(User_Gl,on_delete = CASCADE)
+	age = ForeignKey(User_Gl,on_delete = CASCADE)
+	adress = ForeignKey(User_Gl,on_delete = CASCADE)
+	weight = IntegerField()
+	height = IntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	def __str__(self):
+		return f'{self.name} {self.sername}'
