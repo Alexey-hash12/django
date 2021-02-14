f32bade10f7fbfc4476802b4183aa9d56842e464
Alexey-hash12 <74484313+Alexey-hash12@users.noreply.github.com>
2021-02-14 22:05:35 +0300

    assad

--- main/app/admin.py
+++ main/app/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-
-from .models import User_Gl,Trener,User
-
-
-admin.site.register(User_Gl)
-admin.site.register(Trener)
-admin.site.register(User)
+# from django.contrib import admin
+#
+# from .models import User_Gl,Trener,User
+#
+#
+# # admin.site.register(User_Gl)
+# admin.site.register(Trener)
+# admin.site.register(User)
