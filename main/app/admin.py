3f8cf1f2aa69dcc891c0abb089505709c1189775
Your Name <you@example.com>
2021-02-15 16:36:51 +0300

    asdasd

--- main/app/admin.py
+++ main/app/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
 #
-# from .models import User_Gl,Trener,User
+from .models import User_1,Trener,User_2
 #
 #
-# admin.site.register(User_Gl)
-# admin.site.register(Trener)
-# admin.site.register(User)
+admin.site.register(User_1)
+admin.site.register(Trener)
+admin.site.register(User_2)
--- main/app/models.py
+++ main/app/models.py
@@ -1,33 +1,27 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-#
-# class User_Gl(models.Model):
-# 	name = models.CharField(max_length=300)
-# 	sername = models.CharField(max_length=300)
-# 	othername = models.CharField(max_length=300)
-# 	age = models.IntegerField()
-# 	adress = models.CharField(max_length=300)
-# 	poster = models.ImageField(upload_to = 'poster/')
-#
-# class Trener(models.Model):
-# 	staj = models.IntegerField()
-# 	salary = models.IntegerField()
-# 	time_work = models.TimeField()
-# 	profile = models.OneToOneField(User_Gl, on_delete=models.CASCADE)
-# 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-# 	def __str__(self):
-# 		return f'{self.name} {self.sername}'
-#
-# class User(models.Model):
-# 	poster = models.ForeignKey(User_Gl,on_delete = models.SET_NULL)
-# 	name = models.ForeignKey(User_Gl,on_delete = models.CASCADE)
-# 	sername = models.ForeignKey(User_Gl,on_delete = models.CASCADE)
-# 	othername = models.ForeignKey(User_Gl,on_delete = models.CASCADE)
-# 	age = models.ForeignKey(User_Gl,on_delete = models.CASCADE)
-# 	adress = models.ForeignKey(User_Gl,on_delete = models.CASCADE)
-# 	weight = models.IntegerField()
-# 	height = models.IntegerField()
-# 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-# 	def __str__(self):
-# 		return f'{self.name} {self.sername}'
+from django.db import models
+from django.contrib.auth.models import User
+
+class User_1(models.Model):
+	name = models.CharField(max_length=300)
+	sername = models.CharField(max_length=300)
+	othername = models.CharField(max_length=300)
+	age = models.IntegerField()
+	adress = models.CharField(max_length=300)
+	poster = models.ImageField(upload_to = 'poster/')
+
+class Trener(models.Model):
+	staj = models.IntegerField()
+	salary = models.IntegerField()
+	time_work = models.TimeField()
+	profile = models.OneToOneField(User_1, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	def __str__(self):
+		return f'{self.name} {self.sername}'
+
+class User_2(models.Model):
+	profile = models.OneToOneField(User_1, on_delete=models.CASCADE)
+	weight = models.IntegerField()
+	height = models.IntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	def __str__(self):
+		return f'{self.name} {self.sername}'
